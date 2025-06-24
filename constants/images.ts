5046265ae7eadbb8fb9d8efd4664de817bf7de7e
dianadiv <diana.gs202@gmail.com>
2025-06-24 20:00:18 +0300

    feat: add navigation

---
+++ constants/images.ts
@@ -0,0 +1,9 @@
+import bg from "@/assets/images/bg.png";
+import highlight from "@/assets/images/highlightImage.png";
+import rankingGradient from "@/assets/images/rankingGradient.png";
+
+export const images = {
+  bg,
+  highlight,
+  rankingGradient,
+};
