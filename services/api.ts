499e2146797d0b23c6e01d50b275a721d6d328aa
dianadiv <diana.gs202@gmail.com>
2025-07-01 18:54:52 +0300

    feat: add home page with movies

---
+++ services/api.ts
@@ -0,0 +1,22 @@
+export const CONFIG = {
+  BASE_URL: 'https://api.themoviedb.org/3',
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+  }
+}
+
+export const fetchMovies = async({ query }: { query?: string }) => {
+  const queryString = query ? `/search/movie?query=${query}` : '/discover/movie?sort_by=popularity.desc';
+  const endpoint = `${CONFIG.BASE_URL}${queryString}`
+
+  const response = await fetch(endpoint, { method: 'GET', headers: CONFIG.headers});
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch movies");
+  }
+
+  const data = await response.json();
+  return data.results;
+}
